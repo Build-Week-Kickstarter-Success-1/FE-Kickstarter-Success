aa55306c4b9a1d5cd2621ede4f9f70c7c2ec6e60
Sean Birmingham <seannbirmingham98@gmail.com>
2020-08-25 18:24:34 -0400

    register working

--- crowd_coach/src/Register.js
+++ crowd_coach/src/Register.js
@@ -1,90 +1,88 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from './utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
 
 const blankLogin = {
-    username:'',
-    password:''
-}
+  username: '',
+  password: ''
+};
 
-export default function Login(){
-    const [formValues,setFormValues] = useState(blankLogin)
+export default function Login() {
+  const [formValues, setFormValues] = useState(blankLogin);
+  const history = useHistory();
 
+  const formChange = (evt) => {
+    const { name, value } = evt.target;
 
-    const formChange = (evt) =>{
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
+  };
 
-        const {name,value} = evt.target
+  const postOrder = (register) => {
+    axiosWithAuth()
+      .post('/api/auth/register', register)
+      .then((res) => {
+        console.log(res.data);
+        history.push('/Login');
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setFormValues(blankLogin);
+      });
+  };
 
-        setFormValues({
-            ...formValues,
-            [name]:value
-        })
-    }
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
 
-    const postOrder = register => {
+  const submit = () => {
+    const newLogin = {
+      username: formValues.username.trim(),
+      password: formValues.password.trim()
+    };
+    postOrder(newLogin);
+  };
 
-        axios.post('https://be-lambda-kickstarter-success.herokuapp.com/api/auth/register', register)
-          .then(res => {
-            console.log(register)
-        })
-          .catch(err => {
-            debugger
-            console.log(err)
-          })
-          .finally(() => {
-            setFormValues(blankLogin)
-          })
-      }
-
-      const onSubmit = (evt) =>{
-          evt.preventDefault()
-          submit()
-      }
-
-      const submit = () => {
-        const newLogin = {
-          username: formValues.username.trim(),
-          password: formValues.password.trim(),
-        }
-        postOrder(newLogin)
-      }
-
-
-    return(
-        <div className="form">
-
-            <h2>Sign Up</h2>
-            <form action=""
-            onSubmit={onSubmit}>
-                <label htmlFor="" className="input">Username
-                    <input 
-                    type="text" 
-                    id="username"
-                    name='username'
-                    value={formValues.username}
-                    placeholder='Select a Unique Username'
-                    onChange={formChange}
-                    />
-                </label>
-                <label htmlFor="" className="password">Password
-                    <input 
-                    type="password" 
-                    id="password"
-                    name='password'
-                    value={formValues.password}
-                    onChange={formChange}
-                    />
-                </label>
-                <label className='submit'>
-                    <input
-                    onClick={submit}
-                    type='submit'
-                    id='submitBtn'
-                    name='submitBtn'
-                    value='Register'
-                    />
-                </label>
-            </form>    
-
-        </div>
-    )
+  return (
+    <div className='form'>
+      <h2>Sign Up</h2>
+      <form action='' onSubmit={onSubmit}>
+        <label htmlFor='' className='input'>
+          Username
+          <input
+            type='text'
+            id='username'
+            name='username'
+            value={formValues.username}
+            placeholder='Select a Unique Username'
+            onChange={formChange}
+          />
+        </label>
+        <label htmlFor='' className='password'>
+          Password
+          <input
+            type='password'
+            id='password'
+            name='password'
+            value={formValues.password}
+            onChange={formChange}
+          />
+        </label>
+        <label className='submit'>
+          <input
+            onClick={submit}
+            type='submit'
+            id='submitBtn'
+            name='submitBtn'
+            value='Register'
+          />
+        </label>
+      </form>
+    </div>
+  );
 }
