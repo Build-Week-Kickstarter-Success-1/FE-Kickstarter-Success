c480da2ead355767abbd3278c0f0239db19f6ffd
Sean Birmingham <39016563+sean-birmingham@users.noreply.github.com>
2020-08-27 20:50:39 -0400

    Update Campaigns.js

--- crowd_coach/src/Campaigns.js
+++ crowd_coach/src/Campaigns.js
@@ -1,15 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { axiosWithAuth } from './utils/axiosWithAuth';
 
-
-
 export default function Capmaigns({ details, editCampaign, deleteCampaign }) {
-
-
   if (!details) {
     return <h3>Working fetching your detail&apos;s details...</h3>;
   }
-
 
   return (
     <div className='campaign container'>
@@ -20,21 +15,9 @@ export default function Capmaigns({ details, editCampaign, deleteCampaign }) {
       <p>Category: {details.category}</p>
       <p>Description: {details.description}</p>
       <p>Result: {details.review}</p>
-      
+
       <button onClick={() => editCampaign(details)}>edit</button>
       <button onClick={() => deleteCampaign(details)}>delete</button>
-
     </div>
-{/* 
-            <div className='responnse container'>
-                <h4>Results</h4>
-                <p>Prediction': {result.prediction}</p>
-                <p>Probability of Success: {result.probability_of_success}</p>
-                <p>Monetary Feedback: {result.monetary_feedback}</p>
-                <p>Title Feedback: {result.Title_feedback}</p>
-                <p>Description Feedback: {result.description_feedback}</p>
-                <p>Campaign Time Feedback: {result.campaign_time_feedback}</p>
-                <p>Month Feedback: {result.month_feedback}</p>
-            </div> */}
   );
 }
